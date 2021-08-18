1f0c132a83de9f17769653e94b8e8a04a8f23c8e
hhhung <73517449+hhhung@users.noreply.github.com>
2021-08-18 08:58:26 +0800

    Update project.py

--- project.py
+++ project.py
@@ -4,27 +4,29 @@ from card import Card
 from enemy import Enemy
 from player import Player
 
-difficulty = input(f"Choose Your Difficulty:1. Easy 2. Normal 3. Hard type the corrosponding number to select")
+difficulty = input(f"Choose Your Difficulty: 1. Easy 2. Normal 3. Hard type the corrosponding number to select ")
 if difficulty == "1":
-    goblin = Enemy('Goblin', 7)
+    goblin = Enemy('Goblin', 1)
 elif difficulty == "2":
-    goblin = Enemy('Orc', 12)
+    goblin = Enemy('Orc', 2)
 elif difficulty == "3":
-    goblin = Enemy('Blackguard', 20)
-player = Player('Johan Von Generico', 20)
+    goblin = Enemy('Blackguard', 3)
+player = Player('Johan Von Generico', 30)
 
 for i in range(15):
     player.deck.append(Card('Dagger', 'damage', 4))
 for i in range(2):
     player.deck.append(Card('Dagger but poisoned', 'damage', 6))
-for i in range(8):
+for i in range(4):
     player.deck.append(Card('Healing Word', 'heal', 4))
+for i in range(3):
+    player.deck.append(Card('Shield', 'armour', 6))
 
 random.shuffle(player.deck)
 for i in range(4):
     player.draw()
-
-while player.health > 0 and goblin.health > 0:
+print(f"{player.name} encountered {goblin.name}!")
+while player.health > 0 and goblin.health > 0:    
     print(f"{player.name}'s Turn")
     player.draw()
     player.showcard()
