089a40c1110dc872f3ae0560792d6006cbbfab75
hhhung <73517449+hhhung@users.noreply.github.com>
2021-08-18 08:57:38 +0800

    Update player.py

--- player.py
+++ player.py
@@ -5,8 +5,21 @@ class Player:
         self.cards = []
         self.deck = []
         self.totalhealth = health
+        self.armour = 0
         
     def hit(self, amount):
+        if self.armour == 0:
+            pass
+        elif amount >= self.armour:
+            amount -= self.armour
+            self.armour = 0
+            print("Damage blocked by armour")
+            print("Armour has broken")
+        elif amount < self.armour:
+            self.armour -= amount
+            amount = 0
+            print("Damage blocked completely by armour")
+            print(f"Armour is now {self.armour}")        
         print(f"Hit for {amount}")
         self.health -= amount
         if self.health <= 0:
@@ -27,6 +40,9 @@ class Player:
             enemy.hurt(played_card.value)
         elif played_card.cardType == 'heal':
             self.heal(played_card.value)
+        elif played_card.cardType == 'armour':
+            self.armour += played_card.value
+            print(f"{self.name} has {self.armour} armour")
     def showcard(self):
         print("Your Hand Is: ")        
         for pos, card in enumerate(self.cards):
@@ -36,6 +52,3 @@ class Player:
     def draw(self):
         added_card = self.deck.pop()
         self.cards.append(added_card)
-            
-    
-    
