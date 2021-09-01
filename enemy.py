dc8d8fff93714c456e93cd93a00fac96484dde54
hhhung <73517449+hhhung@users.noreply.github.com>
2021-09-01 10:14:09 +0800

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -6,7 +6,7 @@ class Enemy:
             self.armour = 0
             self.damage = 3
         elif difficulty == 2:
-            self.health = 10
+            self.health = 12
             self.armour = 2
             self.damage = 4
         elif difficulty == 3:
@@ -14,8 +14,12 @@ class Enemy:
             self.armour = 5
             self.damage = 5
         self.cards = []
+        self.original_armour = self.armour
         self.total_health = self.health
         self.fatigue = False
+        self.pause = True
+        if difficulty >= 2:
+            self.spare_armour = True
         
     def hurt(self, amount):
         if self.armour == 0:
@@ -36,16 +40,26 @@ class Enemy:
     def restore(self, amount):
         print(f"{self.name} heals for {amount}")
         self.health += amount
+    def armoured(self, amount):
+        print(f"{self.name} gains armour.")
+        self.armour += amount
         
     def chooseCard(self, player):
         if self.health <= (self.total_health / 2) and self.health >= (self.total_health / 3):
             player.hit(self.damage - 2)
             self.fatigue = False
+            self.pause = False
         elif self.health <= (self.total_health / 3) and self.fatigue == False:
             self.restore(self.damage)
             self.fatigue = True
+            self.pause = False
+        elif difficulty >= 2 and self.armour <= 0 and self.spare_armour == True and self.pause == False:
+            self.armoured(self.original_armour)            
+            self.spare_armour = False
+            self.fatigue = False
         else:
             player.hit(self.damage)
             self.fatigue = False
+            self.pause = False
             
             
